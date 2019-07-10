9dc71073f0da15a4aaad705927c8d51d2d5640b7
misshu1 <31734905+misshu1@users.noreply.github.com>
2019-07-10 09:14:44 +0300

    added map

---
+++ src/components/map/MapCore.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { MapContainer } from "./style";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+const LoadingContainer = () => null;
+
+class MapCore extends Component {
+    render() {
+        return (
+            <MapContainer>
+                <Map
+                    google={this.props.google}
+                    zoom={3}
+                    initialCenter={{
+                        lat: 45.105083,
+                        lng: 24.364982
+                    }}
+                />
+            </MapContainer>
+        );
+    }
+}
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyA8OGJ85mGxQD0gRwJT03Raj6Vdi3RHvS4",
+    LoadingContainer: LoadingContainer
+})(MapCore);
